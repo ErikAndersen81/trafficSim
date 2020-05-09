e64064b74fc83ab29eba6f2f536e7f3be8566226
Erik Andersen <erand17@student.sdu.dk>
2020-05-09 11:52:07 +0200

    Too many changes to remeber

--- server.py
+++ server.py
@@ -1,127 +1,140 @@
-from flask import Flask, escape, request, jsonify
+from flask import Flask, escape, request, jsonify, make_response
 from flask_cors import CORS
-import pandas
+import pandas as pd
 import time
-from datetime import datetime
-
-from filedefs import ints_median_files, ints_mean_files, \
-                     ints_bounds_files, ints_filled_files
+from datetime import datetime, timedelta
+import math
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Create a dicts that maps intersection tags (e.g. 'K124') to dataframes 
-intersections_median = { i[-8:-4]:pandas.read_csv(i) for i in ints_median_files }
-intersections_mean = { i[-8:-4]:pandas.read_csv(i) for i in ints_mean_files }
-intersections_bounds = { i[-8:-4]:pandas.read_csv(i) for i in ints_bounds_files }
-intersections_filled = { i[-8:-4]:pandas.read_csv(i) for i in ints_filled_files }
+class Intersections:
+    """ This singleton class holds references to the data for intersections """
+    def __init__(self):
+        os.chdir("data")
+        subdirs = ['mean_csv', 'median_csv', 'aggregated_csv', 'deviant_csv', 'belows_csv', 'aboves_csv']
+        for subdir in subdirs:
+            os.chdir(subdir)
+            setattr(Intersections, subdir[:-4], { i[:-4]:pd.read_csv(i) for i in os.listdir() })
+            os.chdir("..")
+        setattr(Intersections, "dates", pd.read_csv("dates.csv", parse_dates=[0]).iloc[:,0])
+        os.chdir("..")
+        
 
 public_transit_file = "./data/publicTransit/disturbances_maintenanceformatted.csv"
-public_transit = pandas.read_csv(public_transit_file)
+public_transit = pd.read_csv(public_transit_file, parse_dates=['starttime','endtime'])
+    
+@app.route('/data', methods=['POST'])
+def get_data():
+    jsonData = request.get_json()
+    start = ""
+    end = ""
+    datatypes = []
+    intersections = []
+    disturbances = True
+    outliers = True
+    interval = 1
+    try:
+        start = pd.to_datetime(jsonData.get("starttime", "2015-01-01 00:00:00"))
+        end = pd.to_datetime(jsonData.get("endtime", "2015-02-01 00:00:00"))
+        interval = int((end-start).total_seconds()/(60*15))
+        datatypes = jsonData.get("datatypes", [])
+        intersections = jsonData.get("intersections", [])
+        disturbances = bool(jsonData.get("disturbances", True))
+        outliers = bool(jsonData.get("outliers", True))
+    except:
+        print("Exception Caught")
+        return make_response(jsonify({"error":"Ooops. can't process request!"}),400)
+    
+    bool_mask = ((Intersections.dates >= start) & (Intersections.dates <= end))
+    data = dict()
+    ints_data = {intersection:dict() for intersection in intersections}
+    if 'deviant' in datatypes:
+        ints_data = {intersection:dict() for intersection in Intersections.__dict__['aggregated'].keys()}
+    simplified = False
+    if 'simplified' in datatypes:
+        datatypes.remove('simplified')
+        simplified = True
+    dates = Intersections.dates[bool_mask]
+    dates = dates.astype(str)
+    data['dates'] = list(dates)
+    data['maxVal'] = 0
+    for datatype in datatypes:
+        if datatype in {"mean", "median"}:
+            week_offset = bool_mask.idxmax() % 672
+            first_idx = bool_mask.idxmax()
+            last_idx =  first_idx + interval
+            bools =  bool_mask.iloc[first_idx:last_idx]
+            bools = bools.reset_index(drop=True)
+            weeks = 1
+            if True in bool_mask[::672].value_counts():
+                weeks = bool_mask[::672].value_counts()[True] + 1
+            for k in intersections:
+                df = Intersections.__dict__[datatype][k]
+                df = pd.concat([df for i in range(weeks)], ignore_index=True)
+                df = df.iloc[first_idx:last_idx].reset_index(drop=True)
+                df = df[bools]
+                if simplified:
+                    df = df.sum(axis=1)
+                    data['maxVal'] = max(df.max(), data['maxVal'])
+                    ints_data[k][datatype] = {'summed':list(df)}
+                else:
+                    data['maxVal'] = max(df.max().max(), data['maxVal'])
+                    cols = { col:list(df[col]) for col in df }
+                    ints_data[k][datatype] = cols
+        elif datatype == "deviant": # This is standard and should be sent no matter what to show circles on the map
+            for k in Intersections.__dict__[datatype].keys():
+                df = Intersections.__dict__[datatype][k][bool_mask]
+                ints_data[k][datatype] = sum(df['0'])
+
+                df = Intersections.__dict__['aboves'][k][bool_mask]
+                ints_data[k]['aboves'] = int(df.sum().iloc[0])
+                df = Intersections.__dict__['belows'][k][bool_mask]
+                ints_data[k]['belows'] = int(df.sum().iloc[0])
+                
+                df = Intersections.__dict__['aggregated'][k][bool_mask]
+                ints_data[k]['size'] = df.fillna(value=0).sum().sum()
+        else:
+            for k in intersections:
+                df = Intersections.__dict__[datatype][k][bool_mask]
+                if simplified:
+                    df = df.sum(axis=1)
+                    data['maxVal'] = max(df.max(), data['maxVal'])
+                    ints_data[k][datatype] = {'summed':list(df)}
+                else:
+                    data['maxVal'] = max(df.max().max(), data['maxVal'])
+                    df = df.where(pd.notnull(df), None)
+                    cols = { col:list(df[col]) for col in df }
+                    ints_data[k][datatype] = cols
+            
+    if disturbances:
+        starts_in_timeframe = (public_transit.starttime >= start) & (public_transit.starttime < end)
+        ends_in_timeframe = (public_transit.endtime >= start) & (public_transit.endtime < end)
+        df = public_transit[starts_in_timeframe | ends_in_timeframe]
+        # Calculate the offset from start in 15 minutes
+        x1 = (df['starttime']-start)//pd.Timedelta(minutes=15)
+        # ensure we don't have a negative offset s.t. the graph doesn't overflow to the left.
+        df = df.assign(x1=x1.map(lambda x: max(x,0)))
+        # offset for endtime
+        x2 = (df['endtime']-start)//pd.Timedelta(minutes=15)
+        # shouldn't be bigger than the interval s.t. graph doesn't overflow to the right
+        df = df.assign(x2=x2.map(lambda x: min(x,interval)))
+        df.loc[:,'starttime'] = df['starttime'].astype(str)
+        df.loc[:,'endtime'] = df['endtime'].astype(str)
+        rows = []
+        for idx, row in df.iterrows():
+            lst = [row.location, row.type, row.starttime, row.endtime, row.x1, row.x2]
+            rows.append(lst)
+        data['disturbances'] = rows
+    data['intersections'] = ints_data
+    data['interval'] = interval
+    return jsonify(**data)
 
 @app.route('/')
 def hello():
     return f'Data is being served...'
 
-@app.route('/train', methods=["POST"])
-def get_train():
-    # the data posted must be json containg the key "date".
-    jsonData = request.get_json()
-    if not "date" in jsonData:
-        return jsonify({"data":null, "error":"date field in data not specified"})
-    data = getOngoingDisturbances(jsonData["date"])
-    print(data)
-    return jsonify({"data":data})
-
-def getOngoingDisturbances(date):
-    date = datetime.fromisoformat(date)
-    result = []
-    for row in public_transit.iterrows():
-        start = datetime.fromisoformat(row[1]['starttime'])
-        end = datetime.fromisoformat(row[1]['endtime'])
-        if start < date < end:
-            result.append(row[1].to_json())
-    return result
-
-
-
-@app.route('/<tag>/<form>')
-def get_intersection(tag,form):
-    idx = index(form)
-    if form == "median":
-        return intersections_median[tag].iloc[idx].to_json()
-    else:
-        return intersections_raw[tag].iloc[idx].to_json()
-
-@app.route('/real24h', methods=["POST"])
-def get_real_24h():
-    # the data posted must be json containg the key "date".
-    jsonData = request.get_json()
-    if not "date" in jsonData:
-        return jsonify({"data":None, "error":"date field in data not specified"})
-    date = jsonData["date"]
-    idx = dateToIndex(date)
-    if idx >= len(intersections_filled['K071']):
-        print("No data for date")
-        return jsonify({"error":"No such date in data", "data":None})
-    data = {key:intersection.iloc[idx:idx+672,1:].to_json() for key,intersection in intersections_filled.items()}
-    return jsonify({"index":idx, "date":date, "data":data})
-
-@app.route('/real', methods=["POST"])
-def get_real():
-    # the data posted must be json containg the key "date".
-    jsonData = request.get_json()
-    if not "date" in jsonData:
-        return jsonify({"data":None, "error":"date field in data not specified"})
-    date = jsonData["date"]
-    idx = dateToIndex(date)
-    if idx >= len(intersections_filled['K071']):
-        print("No data for date")
-        return jsonify({"error":"No such date in data", "data":None})
-    data = {key:intersection.iloc[idx,1:].to_json() for key,intersection in intersections_filled.items()}
-    return jsonify({"index":idx, "date":date, "data":data})
-
-@app.route('/bounds', methods=["POST"])
-def get_bounds():
-    # the data posted must be json containg the key "date".
-    jsonData = request.get_json()
-    if not "date" in jsonData:
-        return jsonify({"data":null, "error":"date field in data not specified"})
-    date = jsonData["date"]
-    idx = dateToIndex(date)
-    data = {key:(intersection.iloc[idx,1:]*100).to_json() for key,intersection in intersections_bounds.items()}
-    return jsonify({"index":idx, "date":date, "data":data})
-
-@app.route('/median', methods=["POST"])
-def get_median():
-    # the data posted must be json containg the key "date".
-    jsonData = request.get_json()
-    if not "date" in jsonData:
-        return jsonify({"data":null, "error":"date field in data not specified"})
-    date = jsonData["date"]
-    idx = dateToIndex(date) % 672
-    data = {key:intersection.iloc[idx,1:].to_json() for key,intersection in intersections_median.items()}
-    return jsonify({"index":idx, "date":date, "data":data})
-
-@app.route('/mean', methods=["POST"])
-def get_mean():
-    # the data posted must be json containg the key "date".
-    jsonData = request.get_json()
-    if not "date" in jsonData:
-        return jsonify({"data":null, "error":"date field in data not specified"})
-    date = jsonData["date"]
-    idx = dateToIndex(date) % 672
-    data = {key:intersection.iloc[idx,1:].to_json() for key,intersection in intersections_mean.items()}
-    return jsonify({"index":idx, "date":date, "data":data})
-
-def dateToIndex(dateString):
-    # The first entry in every filled_ file has the time stamp '2017-02-06 13:15:00'.
-    startDate = datetime(2017, 2, 6, hour=13, minute=15)
-    getDate = datetime.fromisoformat(dateString)
-    delta = getDate-startDate
-    # Each entry corresponds to 15 minutes of 60 seconds each
-    idx = int(delta.total_seconds()/(60*15))
-    return idx
-
 if __name__=="__main__":
-    app.run()
+    Intersections() # load data for the intersections
+    app.run(host='192.168.1.86')
